86402c7f5e937d53d27824957bbe05500b341602
Felipe Milary Dias <milarydesousa@outlook.com>
2020-07-27 22:57:17 -0300

    Setup inicial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './V-V2-en.jpg'
+import log1 from './foto.jpg'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        <img src={log1} className="App-logo" alt="logo"/>
+        <p>
+          Minha pagina
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
